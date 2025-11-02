33bb94722b68dffd10b4dd9f37e77f2b055b93d0
Christopher Troublefield <ffson1999@yahoo.com>
2025-11-02 13:19:58 -0500

    placed navbar properly in App.jsx

--- runetrack/client/src/App.jsx
+++ runetrack/client/src/App.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
-    <Routes>
+    <>
       <Navbar />
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
   );
 }
