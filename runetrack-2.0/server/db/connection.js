7d5dfcc2da97dc65f09c07de05f86acd3fa4bcf8
Christopher Troublefield <ffson1999@yahoo.com>
2025-11-16 08:41:41 -0500

    remobed mongodb connection

--- runetrack-2.0/server/db/connection.js
+++ runetrack-2.0/server/db/connection.js
@@ -3,11 +3,6 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 
 const uri = process.env.ATLAS_URI || "";
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
   tlsAllowInvalidCertificates: true,
   tlsAllowInvalidHostnames: true,
 });
